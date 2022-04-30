209794e0a28992aab69e5454d15b3357e9401861
MinYuliadi <irfan.nugraha404@gmail.com>
2022-04-30 21:45:49 +0800

    pembaruan about page

--- mini-game/src/component/CardAboutComponent.js
+++ mini-game/src/component/CardAboutComponent.js
@@ -8,13 +8,13 @@ const CardAboutComponent = (props) => {
         <span>Usia : {props.age} </span>
       <div className='flex gap-3 mt-3 w-full items-center justify-center'>
       <a href={`${props.instagram}`}>
-        <img className='w-10 h-10' src='https://th.bing.com/th/id/R.14fbf1be3bb79444c0245a51a0f89ab2?rik=UkXeB5L2aE8Biw&riu=http%3a%2f%2fpngimg.com%2fuploads%2finstagram%2finstagram_PNG9.png&ehk=GnhAoqBr5eV3LRtLGDB2XElOW7mvkdk29%2byVkqj09y8%3d&risl=&pid=ImgRaw&r=0' />
+        <img className='w-10 h-10' alt='' src='https://th.bing.com/th/id/R.14fbf1be3bb79444c0245a51a0f89ab2?rik=UkXeB5L2aE8Biw&riu=http%3a%2f%2fpngimg.com%2fuploads%2finstagram%2finstagram_PNG9.png&ehk=GnhAoqBr5eV3LRtLGDB2XElOW7mvkdk29%2byVkqj09y8%3d&risl=&pid=ImgRaw&r=0' />
       </a>
       <a href={`${props.whatsapp}`}>
-        <img className='w-10 h-10' src='https://th.bing.com/th/id/R.3db05f40f9bfbfa4818e5f841359ac18?rik=i9uCGc2yoCWfNA&riu=http%3a%2f%2fspeedyclearance.uk%2fwp-content%2fuploads%2f2018%2f04%2fwhatsapp-icon.png&ehk=%2fGSNSk4y8vLd2qCiosXRI0WSYOth7SLdJewCXSxpcmY%3d&risl=&pid=ImgRaw&r=0' />
+        <img className='w-10 h-10' alt='' src='https://th.bing.com/th/id/R.3db05f40f9bfbfa4818e5f841359ac18?rik=i9uCGc2yoCWfNA&riu=http%3a%2f%2fspeedyclearance.uk%2fwp-content%2fuploads%2f2018%2f04%2fwhatsapp-icon.png&ehk=%2fGSNSk4y8vLd2qCiosXRI0WSYOth7SLdJewCXSxpcmY%3d&risl=&pid=ImgRaw&r=0' />
       </a>
       <a href={`${props.github}`}>
-        <img className='w-10 h-10' src='https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png' />
+        <img className='w-10 h-10' src='https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png' alt='' />
       </a>
       </div>
     </div>
